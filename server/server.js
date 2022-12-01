72629435c43a968b4b0a56e1ce9bef9fd8cccfa2
herald-of-spring <anhnguye@usc.edu>
2022-11-30 21:12:46 -0800

    updated server to use apollo

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,9 +15,21 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+const server = new ApolloServer({
+  typeDefs, 
+  resolvers,
+  context: authMiddleware,
 });
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+  db.once('open', () => { 
+    app.listen(PORT, () => { 
+      console.log(`API server running on port ${PORT}!`); 
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`); 
+    }) 
+  }) 
+};
+
+startApolloServer(typeDefs, resolvers);
